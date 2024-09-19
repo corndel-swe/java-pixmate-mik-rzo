7b66c8ee3d4bfa885185f6bfb2791bf258056775
mik-rzo <michael.panong3@gmail.com>
2024-09-19 10:49:10 +0100

    solves 1a

--- src/main/java/com/corndel/pixmate/exercises/Exercise1a.java
+++ src/main/java/com/corndel/pixmate/exercises/Exercise1a.java
@@ -16,7 +16,10 @@ public class Exercise1a {
    * @return the sum of the even numbers smaller than or equal to n
    */
   public static int addEven(int n) {
-    // TODO
-    return 0;
+    int sum = 0;
+    for (int i = 2; i <= n; i += 2 ) {
+      sum += i;
+    }
+    return sum;
   }
 }
